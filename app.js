03e57561828533f6d72c1fc0b29f6d8e6e87fa89
gabelatham <78877534+gabelatham@users.noreply.github.com>
2024-03-27 20:38:48 -0500

    Completed Part 7

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var zombiesRouter = require('./routes/zombies');
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/zombies', zombiesRouter);
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
